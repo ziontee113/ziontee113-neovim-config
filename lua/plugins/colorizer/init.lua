193ee2e842464ad69ce435fb38035ad45feed2fa
ziontee113 <ziontee113@gmail.com>
2022-10-18 20:44:39 +0700

    Set colorizer's TSX Tailwind setting to foreground

--- lua/plugins/colorizer/init.lua
+++ lua/plugins/colorizer/init.lua
@@ -1,1 +1,6 @@
-require 'colorizer'.setup()
+require("colorizer").setup({
+	filetypes = {
+		"*", -- Highlight all files, but customize some others.
+		typescriptreact = { mode = "foreground", tailwind = true },
+	},
+})
