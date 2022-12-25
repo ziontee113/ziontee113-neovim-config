17e76fd2c8de2ab33320872ae53e7afffb2d7a8d
ziontee113 <ziontee113@gmail.com>
2022-12-25 14:08:58 +0700

    Refactor Svelte tag TS styling

--- after/queries/svelte/highlights.scm
+++ after/queries/svelte/highlights.scm
@@ -1,12 +1,7 @@
 ;; extends
 
-((start_tag
-    ("<" @keyword)
-    ((tag_name) @keyword @cap (#any-of? @cap "main" "style" "script"))
-    (">" @keyword)
-))
-((end_tag
-    ("</" @keyword)
+((_
+    ([ "<" "</" ] @keyword)
     ((tag_name) @keyword @cap (#any-of? @cap "main" "style" "script"))
     (">" @keyword)
 ))
